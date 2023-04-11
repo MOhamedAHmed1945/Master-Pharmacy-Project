e963a1f966e36526efcf5158af04e1b6c74791bc
Mohamed Ahmed <115259098+MOhamedAHmed1945@users.noreply.github.com>
2023-04-11 23:01:34 +0200

    initial commit

---
+++ lib/models/model_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+import 'data_product_model.dart';
+
+class ModelProvider with ChangeNotifier {
+  List selectedProduct = [];
+  List selectedProductOrders = [];
+  int productPrice = 0;
+
+  add(DataProductModel product) {
+    selectedProduct.add(product);
+
+    productPrice += product.productPrice.round();
+
+    notifyListeners();
+  }
+
+  remove(DataProductModel product) {
+    selectedProduct.remove(product);
+
+    productPrice -= product.productPrice.round();
+
+    notifyListeners();
+  }
+
+  get itemModelLength {
+    return selectedProduct.length;
+  }
+
+  addOrder(DataProductModel productOrder) {
+    selectedProductOrders.add(productOrder);
+
+    notifyListeners();
+  }
+// moveToOrderScreen it is temporary
+  void moveToOrderScreen() {
+    for (var product in selectedProduct) {
+      addOrder(product);
+    }
+    notifyListeners();
+  }
+
+  removeItemOfList() {
+    selectedProduct.clear();
+    productPrice = 0;
+    notifyListeners();
+  }
+}
